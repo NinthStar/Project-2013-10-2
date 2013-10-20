e532c6f81f605b6fb4dc5d685e8c29903772b1e0
叶嘉楠 <hdt_3s@qq.com>
2013-10-20 19:47:34 +0800

    Add "Time" and "Game" statement

--- Terminal.js
+++ Terminal.js
@@ -1,3 +1,8 @@
+var MaxNum = 1000;
+
+var guessNum = -1,
+	lowerBound, upperBound;
+
 function createNewLine()
 {
 	// Add new line
@@ -22,30 +27,73 @@ function response(inputText)
 	// Response to the input data
 	var newChild = document.createElement("div");
 	newChild.className = "system";
-	
-	switch (inputText.toLowerCase())
+	if (guessNum === -1)
 	{
-	case "":
-		break;
-	case "help":
-		newChild.innerHTML = "Command List :<br/>"
-		                   + "->help..: Get command list.<br/>"
-						   + "->hello.: Say \"Hello\" to the system.<br/>"
-						   + "->exit..: Close the page.<br/>"
-		break;
-	case "hello":
-		newChild.innerHTML = "Hello~!<br/>";
-		break;
-	case "time":
-		newChild.innerHTML = "<br/>";
-		break;
-	case "exit":
-		
-		break;
-	default:
-		newChild.innerHTML = "Unknown command. You can use \"help\" to get command list.<br/>"
-		break;
+		switch (inputText.toLowerCase())
+		{
+		case "":
+			break;
+		case "game":
+			// Set number
+			do
+			{
+				guessNum = Math.floor(Math.random() * MaxNum);
+			}while (guessNum === 0);
+			
+			// Set Bound
+			lowerBound = 0;
+			upperBound = MaxNum;
+			
+			newChild.innerHTML = "The number is in (0, 1000).<br/>";
+			
+			break;
+		case "help":
+			newChild.innerHTML = "Command List :<br/>"
+							   + "->game..: Play a mini game!<br/>"
+							   + "->help..: Get command list.<br/>"
+							   + "->hello.: Say \"Hello\" to the system.<br/>"
+							   + "->time..: Get Current Time.<br/>"
+							   + "->exit..: Close the page.<br/>"
+			break;
+		case "hello":
+			newChild.innerHTML = "Hello~!<br/>";
+			break;
+		case "time":
+			var date = new Date();
+			newChild.innerHTML = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + "<br/>";
+			break;
+		case "exit":
+			// Do not work in Firefox
+			window.open('','_parent','');
+			window.close();
+			break;
+		default:
+			newChild.innerHTML = "Unknown command. You can use \"help\" to get command list.<br/>"
+			break;
+		}
 	}
+	else
+	{
+		// Minigame
+		var userInput = parseInt(inputText);
+		
+		// Get the number
+		if (guessNum === userInput)
+		{
+			newChild.innerHTML = "You get it! The number is " + guessNum + "!<br/>";
+			guessNum = -1;
+		}
+		// Do not get the number
+		else
+		{
+			if (userInput > lowerBound && userInput < guessNum)
+				lowerBound = userInput;
+			else if (userInput < upperBound && userInput > guessNum)
+				upperBound = userInput;
+			newChild.innerHTML = "No! The number is in (" + lowerBound + ", " + upperBound + ").<br/>";
+		}
+	}
+	
 	document.body.appendChild(newChild);
 }
 
@@ -55,7 +103,7 @@ function detectEnter(event)
 	{
 		// Get input data
 		var lastInput = document.getElementById("input"),
-		    inputText = lastInput.value,
+			inputText = lastInput.value,
 			newChild;
 		
 		// Change input data into div
@@ -64,7 +112,7 @@ function detectEnter(event)
 		newChild.className = "input";
 		newChild.innerHTML = inputText + "<br/>";
 		document.body.appendChild(newChild);
-			
+		
 		// Response to the input data
 		response(inputText);
 		
